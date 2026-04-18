7ee707b675f9dd4ed5c0b6604daf7c136c4aaa1d
snowykte0426 <snowykte0426@naver.com>
2026-04-18 23:19:19 +0900

    fix(cowork-preference): vertx-service-discovery-backend-eureka 미존재 의존성 제거

--- cowork-preference/build.gradle.kts
+++ cowork-preference/build.gradle.kts
@@ -24,7 +24,8 @@ dependencies {
     implementation("io.vertx:vertx-redis-client")
     implementation("io.vertx:vertx-kafka-client")
     implementation("io.vertx:vertx-service-discovery")
-    implementation("io.vertx:vertx-service-discovery-backend-eureka")
+    // TODO: Eureka 통합 구현 시 HTTP Client로 직접 Eureka REST API 연동 필요
+    //       vertx-service-discovery-backend-eureka는 Vert.x 4.x에서 별도 artifact 미제공
 
     // Flyway (JDBC, 시작 시 블로킹 실행)
     implementation("org.flywaydb:flyway-core:10.15.0")
