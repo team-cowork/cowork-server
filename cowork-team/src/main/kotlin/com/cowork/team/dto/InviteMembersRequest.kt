126af32d5a6374317aac880c6efaf60a93d4bb01
snowykte0426 <snowykte0426@naver.com>
2026-04-20 18:13:56 +0900

    feat(team): springdoc Swagger 어노테이션 추가 및 게이트웨이 문서 라우트 설정

--- cowork-team/src/main/kotlin/com/cowork/team/dto/InviteMembersRequest.kt
+++ cowork-team/src/main/kotlin/com/cowork/team/dto/InviteMembersRequest.kt
@@ -1,5 +1,8 @@
 package com.cowork.team.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class InviteMembersRequest(
+    @Schema(description = "초대할 사용자 ID 목록", example = "[2, 3, 4]", required = true)
     val userIds: List<Long>,
 )
