2c2e877730d86a61ae9514249d1fdaa10c2ab95b
snowykte0426 <snowykte0426@naver.com>
2026-04-20 11:07:07 +0900

    update(cowork-gateway): Swagger UI 통합 및 보안 설정 업데이트

--- cowork-gateway/build.gradle.kts
+++ cowork-gateway/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.8")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
