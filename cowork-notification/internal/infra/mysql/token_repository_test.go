c29523826a1b89c394b3cce66a8a2fb9aab36a42
황지훈 <wangjajihooon@gmail.com>
2026-04-21 08:15:56 +0900

    fix(notification): return NotFound on missing token delete, clean up test

--- cowork-notification/internal/infra/mysql/token_repository_test.go
+++ cowork-notification/internal/infra/mysql/token_repository_test.go
@@ -5,10 +5,8 @@ import (
 
 	"github.com/cowork/cowork-notification/internal/domain/token"
 	"github.com/cowork/cowork-notification/internal/infra/mysql"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestTokenRepository_ImplementsInterface(t *testing.T) {
 	var _ token.Repository = (*mysql.TokenRepository)(nil)
-	assert.True(t, true)
 }
