dab549b07bab2250fcce6495bdd0713830221e87
jong-yun <s25069@gsm.hs.kr>
2026-04-21 10:17:21 +0900

    docs(chat): DTO 필드 Swagger @ApiProperty 데코레이터 추가

--- cowork-chat/src/chat/dto/edit-message.dto.ts
+++ cowork-chat/src/chat/dto/edit-message.dto.ts
@@ -1,5 +1,7 @@
 import { IsString, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class EditMessageDto {
+    @ApiProperty({ description: '수정할 메시지 내용 (최대 25000자)', maxLength: 25000 })
     @IsString() @MaxLength(25000) content!: string;
 }
