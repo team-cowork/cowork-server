adc1840f0dbdbcf6a0c4ed0a716becab47255de4
snowykte0426 <snowykte0426@naver.com>
2026-04-23 14:16:28 +0900

    update(preference): 메트릭 등록 방식 및 스키마 인덱스 추가

--- cowork-preference/src/main/kotlin/com/cowork/preference/MetricsRegistry.kt
+++ cowork-preference/src/main/kotlin/com/cowork/preference/MetricsRegistry.kt
@@ -1,6 +1,6 @@
 package com.cowork.preference
 
-import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
+import io.micrometer.prometheus.PrometheusMeterRegistry
 
 object MetricsRegistry {
     lateinit var registry: PrometheusMeterRegistry
