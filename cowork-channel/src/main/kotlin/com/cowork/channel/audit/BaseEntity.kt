ee7bb29bc379a5c9c0b1aef7d132a8928fb27327
snowykte0426 <snowykte0426@naver.com>
2026-04-30 12:01:17 +0900

    add(channel): JPA Auditing 기능 추가

---
+++ cowork-channel/src/main/kotlin/com/cowork/channel/audit/BaseEntity.kt
@@ -0,0 +1,32 @@
+package com.cowork.channel.audit
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    lateinit var createdAt: LocalDateTime
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    lateinit var updatedAt: LocalDateTime
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    var createdBy: Long? = null
+
+    @LastModifiedBy
+    @Column(name = "last_modified_by")
+    var lastModifiedBy: Long? = null
+}
