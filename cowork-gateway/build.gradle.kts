7bdb7b5d668a502fc1e234b0f1f4c842fb4922b0
jyx-07 <s25069@gsm.hs.kr>
2026-05-13 13:21:07 +0900

    chore: release v20260513.1

--- cowork-gateway/build.gradle.kts
+++ cowork-gateway/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.cowork"
-version = "20260513.0"
+version = "20260513.1"
 
 java {
     toolchain {
--- cowork-notification/cmd/server/main.go
+++ cowork-notification/cmd/server/main.go
@@ -1,5 +1,5 @@
 // @title          cowork-notification API
-// @version        20260513.0
+// @version        20260513.1
 // @description    FCM 디바이스 토큰 관리 및 푸시 알림 서비스
 // @BasePath       /
 package main
