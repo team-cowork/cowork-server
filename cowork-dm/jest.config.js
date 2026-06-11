0117f39142fc199f0501e36a25b37b00c477d7c3
Sean-mn <b01080080329@gmail.com>
2026-06-12 01:39:05 +0900

    refactor(dm): cowork-dm 모듈 제거 및 게이트웨이 라우팅 교체

--- cowork-dm/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    rootDir: 'src',
-    testRegex: '.*\\.spec\\.ts$',
-    moduleFileExtensions: ['js', 'json', 'ts'],
-    collectCoverageFrom: ['**/*.(t|j)s'],
-    coverageDirectory: '../coverage',
-};
--- cowork-dm/src/message/dto/edit-message.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, MaxLength } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class EditMessageDto {
-    @ApiProperty()
-    @IsString()
-    @MaxLength(25000)
-    content!: string;
-}
