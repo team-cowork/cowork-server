f342c0ecb4814af6d3f1c2b1bf4b5defe9e7811b
jyx-07 <s25069@gsm.hs.kr>
2026-06-19 08:18:34 +0900

    fix(chat): BlockModule ConfigModule 누락 추가

--- cowork-chat/src/block/block.module.ts
+++ cowork-chat/src/block/block.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { BlockRedis } from './block.redis';
 import { BlockService } from './block.service';
 import { BlockController } from './block.controller';
 
 @Module({
+    imports: [ConfigModule],
     controllers: [BlockController],
     providers: [BlockRedis, BlockService],
     exports: [BlockService],
