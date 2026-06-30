b95a2afe729a0531850be1e166936f7194834593
황지훈 <wangjajihooon@gmail.com>
2026-06-30 12:07:31 +0900

    fix(channel): deprecated Kafka 직렬화 클래스 교체 및 불필요한 non-null 단언 제거

--- cowork-channel/src/main/kotlin/com/cowork/channel/global/config/KafkaProducerConfig.kt
+++ cowork-channel/src/main/kotlin/com/cowork/channel/global/config/KafkaProducerConfig.kt
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
-import org.springframework.kafka.support.serializer.JsonSerializer
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer
 
 @Configuration
 class KafkaProducerConfig(private val kafkaProperties: KafkaProperties) {
@@ -17,8 +17,8 @@ class KafkaProducerConfig(private val kafkaProperties: KafkaProperties) {
     fun producerFactory(): ProducerFactory<String, Any> {
         val props = kafkaProperties.buildProducerProperties().toMutableMap<String, Any>()
         props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
-        props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
-        props[JsonSerializer.ADD_TYPE_INFO_HEADERS] = false
+        props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JacksonJsonSerializer::class.java
+        props[JacksonJsonSerializer.ADD_TYPE_INFO_HEADERS] = false
         return DefaultKafkaProducerFactory(props)
     }
 
