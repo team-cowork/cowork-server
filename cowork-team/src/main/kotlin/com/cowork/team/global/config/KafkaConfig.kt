a5f13b0363ff57b1a68e4f0a572a4137380c6410
황지훈 <wangjajihooon@gmail.com>
2026-06-30 12:07:36 +0900

    fix(team): deprecated Kafka 직렬화 클래스 및 HttpStatus 상수 교체

--- cowork-team/src/main/kotlin/com/cowork/team/global/config/KafkaConfig.kt
+++ cowork-team/src/main/kotlin/com/cowork/team/global/config/KafkaConfig.kt
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
-import org.springframework.kafka.support.serializer.JsonSerializer
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer
 
 @Configuration
 class KafkaConfig(private val kafkaProperties: KafkaProperties) {
@@ -18,8 +18,8 @@ class KafkaConfig(private val kafkaProperties: KafkaProperties) {
     fun producerFactory(): ProducerFactory<String, TeamEventPayload> {
         val props = kafkaProperties.buildProducerProperties().toMutableMap<String, Any>()
         props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
-        props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
-        props[JsonSerializer.ADD_TYPE_INFO_HEADERS] = false
+        props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JacksonJsonSerializer::class.java
+        props[JacksonJsonSerializer.ADD_TYPE_INFO_HEADERS] = false
         return DefaultKafkaProducerFactory(props)
     }
 
