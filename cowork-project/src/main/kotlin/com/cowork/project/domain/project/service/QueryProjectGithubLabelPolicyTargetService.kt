b3cdbbafd18fcdf30128051fcbd53a0e4056bc8c
wwwcomcomcomcom <68013923+wwwcomcomcomcom@users.noreply.github.com>
2026-08-27 15:17:10 +0900

    update(project): GitHub 연동 내부 조회 API와 검증기 제거

--- cowork-project/src/main/kotlin/com/cowork/project/domain/project/service/QueryProjectGithubLabelPolicyTargetService.kt
+++
@@ -1,7 +0,0 @@
-package com.cowork.project.domain.project.service
-
-import com.cowork.project.domain.project.presentation.data.response.ProjectGithubLabelPolicyTargetResDto
-
-interface QueryProjectGithubLabelPolicyTargetService {
-    fun execute(owner: String, repo: String): List<ProjectGithubLabelPolicyTargetResDto>
-}
