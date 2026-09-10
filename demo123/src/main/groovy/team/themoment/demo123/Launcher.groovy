b9ef9b3521751df3e3e2f3d5edb08913bcd7e55b
전준연 <junjuny0227@gmail.com>
2026-09-10 14:17:25 +0900

    update(demo): JavaFX 예제 애플리케이션 제거

--- demo123/src/main/groovy/team/themoment/demo123/Launcher.groovy
+++
@@ -1,9 +0,0 @@
-package team.themoment.demo123
-
-import javafx.application.Application
-
-class Launcher {
-    static void main(String[] args) {
-        Application.launch(HelloApplication.class, args)
-    }
-}
