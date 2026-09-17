f1ade78a8ab2b680551e7f7bf9ef589c45c20f7f
박승일 (Park Seungil) <s24060@gsm.hs.kr>
2026-09-16 23:11:36 -0700

    update(user): Elixir 의존성 최신화 및 Cowlib Hex 전환

--- cowork-user/lib/cowork_user/open_api.ex
+++ cowork-user/lib/cowork_user/open_api.ex
@@ -177,11 +177,11 @@ defmodule CoworkUser.OpenAPI do
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>cowork-user Swagger UI</title>
-      <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+      <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.33.0/swagger-ui.css" />
     </head>
     <body>
       <div id="swagger-ui"></div>
-      <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+      <script src="https://unpkg.com/swagger-ui-dist@5.33.0/swagger-ui-bundle.js"></script>
       <script>
         window.ui = SwaggerUIBundle({
           url: "/v3/api-docs",
